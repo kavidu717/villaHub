67e90f661a2a9dd4ee2c2a6f8fcc8989c35994a3
kavidu717 <dushmanthakavidu143@gmail.com>
2026-04-20 18:07:01 +0530

    add the auth middleware

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -128,3 +128,4 @@ export const loginUser = async (req, res) => {
   }
 }
 
+
---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+import jsonwebtoken from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+
+
+export const protect = async (req, res, next) => {
+    let token;
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            token = req.headers.authorization.split(" ")[1];
+
+            const decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET_KEY);
+
+            req.user = await User.findById(decoded.id).select("-password");
+
+            next();
+            } catch (error) {
+                console.error(error);
+                res.status(401);
+                throw new Error("Not authorized");
+            }
+        }
+
+        if (!token) {
+            res.status(401);
+            throw new Error("Not authorized, no token");
+        }
+            
+    
+}
