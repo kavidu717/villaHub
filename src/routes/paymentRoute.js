482a8b3c3d694524557856a2f10d342c4adabc21
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-03 00:18:59 +0530

    fix payment route error

--- src/routes/paymentRoute.js
+++ src/routes/paymentRoute.js
@@ -32,8 +32,8 @@ router.post("/stripe", async (req, res) => {
       ],
 
       // Pass booking ID to success page
-      success_url: `https://villa-hub-frontend.vercel.app/success?bookingId=${orderId}`,
-      cancel_url: "https://villa-hub-frontend.vercel.app/cancel",
+      success_url: `https://villa-hub-front.vercel.app/success?bookingId=${orderId}`,
+      cancel_url: "https://villa-hub-front.vercel.app/cancel",
     });
 
     res.json({
